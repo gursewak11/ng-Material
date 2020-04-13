65929d2637584fa44a578b1dc70e8c6156ce268f
gursewak11 <rainugursewaksingh@gmail.com>
2020-04-13 08:17:04 +0530

    Adding button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TypographyComponent } from './components/typography/typography.component'; 
+import { ButtonComponent } from './components/button/button.component';
+import { ButtonToggleComponent } from './components/button-toggle/button-toggle.component';
 
 const routes: Routes = [
-  {path: 'typography', component: TypographyComponent}
+  {path: 'typography', component: TypographyComponent},
+  {path: 'button', component: ButtonComponent},
+  {path: 'buttontoggle', component: ButtonToggleComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './modules/material/material.module';
 import { TypographyComponent } from './components/typography/typography.component';
+import { ButtonComponent } from './components/button/button.component';
+import { ButtonToggleComponent } from './components/button-toggle/button-toggle.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TypographyComponent
+    TypographyComponent,
+    ButtonComponent,
+    ButtonToggleComponent
   ],
   imports: [
     BrowserModule,
