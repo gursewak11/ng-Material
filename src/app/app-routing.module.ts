0a3a5baac77bfa88545c2897a6fa06d2ee079848
gursewak11 <rainugursewaksingh@gmail.com>
2020-04-13 06:13:47 +0530

    typography

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TypographyComponent } from './components/typography/typography.component'; 
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'typography', component: TypographyComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './modules/material/material.module';
+import { TypographyComponent } from './components/typography/typography.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TypographyComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule, 
+    MaterialModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
