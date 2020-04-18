af995e27ec8ce467ff96166f116bd14a72bdcfde
gursewak11 <rainugursewaksingh@gmail.com>
2020-04-18 12:00:21 +0530

    adding select

--- src/app/components/forms/select/select.component.ts
+++ src/app/components/forms/select/select.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-select',
   templateUrl: './select.component.html',
-  styleUrls: ['./select.component.css']
+  styleUrls: ['./select.component.css'],
 })
 export class SelectComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
+  selectedValue: string;
+  ngOnInit(): void {}
 }
